84ef50662041e29fe17c785653d0a8991cbe382f
Lorena Costa <lorenacosta@ucm.es>
2018-12-10 18:17:27 +0100

    Protegiendo la API de fotos con clases de permisos genéricas. 114

--- photos/api.py
+++ photos/api.py
@@ -2,11 +2,15 @@
 from photos.models import Photo
 from photos.serializers import PhotoSerializer, PhotoListSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class PhotoListAPI(ListCreateAPIView):
     # Le decimos cual es nuestro modelo y cual es nuestro serializador
     queryset = Photo.objects.all()
+
+    # Para autenticacion
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     # Para el detalle de la foto, porque sin esto, el POST no funciona. Dependiendo del método (GET o POST) utilizaremos
     # un serializer u otro.
@@ -25,3 +29,6 @@ class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
     # Le decimos cual es nuestro modelo y cual es nuestro serializador
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+
+    # Para autenticacion
+    permission_classes = (IsAuthenticatedOrReadOnly,)
